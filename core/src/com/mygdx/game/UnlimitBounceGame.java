f8362336df3d785a8658ed97c1c638a84d2fe65d
pawissanutt <pawissanutt@gmail.com>
2016-10-14 13:56:02 +0700

    start

---
+++ core/src/com/mygdx/game/UnlimitBounceGame.java
@@ -0,0 +1,28 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class UnlimitBounceGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	public static final int HEIGHT = 600;
+    public static final int WIDTH = 800;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
