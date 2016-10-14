263582fecc51968d7a8674a392934d8d72abde9b
pawissanutt <pawissanutt@gmail.com>
2016-10-14 14:29:28 +0700

    picture

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,7 +1,23 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 
 public class GameScreen extends ScreenAdapter{
 
+	 @Override
+	    public void render(float delta) {
+	    	update( delta);
+	    //	world.update(delta);
+	    	Gdx.gl.glClearColor(0, 0, 0, 1);
+	        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+	    //  worldRenderer.render(delta);
+	    }
+	    
+	    private void update(float delta) {
+//	        updatePacmanDirection();
+//	        world.update(delta);
+	    }
 }
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.mygdx.game.UnlimitBounceGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = UnlimitBounceGame.WIDTH;
+        config.height = UnlimitBounceGame.HEIGHT;
 		new LwjglApplication(new UnlimitBounceGame(), config);
 	}
 }
